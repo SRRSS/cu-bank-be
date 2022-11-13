1e8c3fca84b1a24de884d24a9a3028b5b3852e6c
Saridraj <saridrajsapsin@gmail.com>
2022-11-13 21:43:29 +0700

    cors

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ const corsOptions = {
   };
 //Enable CORS
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 //route files
