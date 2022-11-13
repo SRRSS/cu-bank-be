a3f8ade19e5ddfffe763b171fa1fbd6048fa71ea
Saridraj <saridrajsapsin@gmail.com>
2022-11-13 14:14:35 +0700

    set port

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use('/api/v1/auth',auth);
 
 
 
-const PORT=process.env.PORT || 4000;
+const PORT=process.env.PORT || 8080;
 
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
